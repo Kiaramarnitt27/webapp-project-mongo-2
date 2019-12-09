a291ed91c7bb84dbe1c860e5eb22e57e71540842
Kiaramarnitt27 <54841049+Kiaramarnitt27@users.noreply.github.com>
2019-12-10 00:16:56 +0200

    Database Schema
    Created user schema for regisstartion

--- routes/signin.js
+++ routes/signin.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/db");
 
 // Good validation documentation available at https://express-validator.github.io/docs/
 const { sanitizeBody } = require("express-validator");
@@ -14,14 +15,52 @@ router.post(
     .trim()
     .escape(),
   function(req, res, next) {
-    var name = req.body.name;
-    var user = req.body.user;
-    var email = req.body.email;
-    var password = req.body.password;
-    var password2 = req.body.password2;
+    const { name, username, email, password, password2 } = req.body;
+    let errors = [];
 
-    //Redirect (piece of shit that dosnt wanna work!)
-    res.redirect("/signin");
+    if (!name || !username || !email || !password || !password2) {
+      errors.push({ msg: "Please enter all fields" });
+    }
+
+    if (password !== password2) {
+      errors.push({ msg: "Passwords do not match" });
+    }
+
+    if (password.length < 8) {
+      errors.push({ msg: "Password must be at least 8 characters" });
+    }
+
+    if (errors.length > 0) {
+      res.render("signin", {
+        errors,
+        name,
+        username,
+        email,
+        password,
+        password2
+      });
+    } else {
+      User.findOne({ email: email }).then(user => {
+        if (user) {
+          errors.push({ msg: "Email already exists" });
+          res.render("signin", {
+            errors,
+            name,
+            username,
+            email,
+            password,
+            password2
+          });
+        } else {
+          const newUser = new User({
+            name,
+            username,
+            email,
+            password
+          });
+        }
+      });
+    }
   }
 );
 
